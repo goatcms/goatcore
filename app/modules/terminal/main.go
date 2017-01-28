d845258bbd94fedbd32e4f46816b6924475dd744
Sebastian Pozoga <sebastian@pozoga.eu>
2017-01-28 12:09:33 +0100

    rewrite terminal module

--- app/modules/terminal/main.go
+++ app/modules/terminal/main.go
@@ -7,28 +7,30 @@ import (
 	"github.com/goatcms/goat-core/app"
 )
 
-// ModuleDependencies is a dependencies for the module
-type ModuleDependencies struct {
-	Name         string    `app:"AppName"`
-	Version      string    `app:"AppVersion"`
-	Welcome      string    `app:"?AppWelcome"`
-	Company      string    `app:"?AppCompany"`
-	GoatVersion  string    `engine:"GoatVersion"`
-	CommandScope app.Scope `global:"CommandScope"`
-}
-
 // Module is command unit
 type Module struct {
-	dependencies ModuleDependencies
+	app          app.App
+	dependencies struct {
+		Name         string    `app:"AppName"`
+		Version      string    `app:"AppVersion"`
+		Welcome      string    `app:"?AppWelcome"`
+		Company      string    `app:"?AppCompany"`
+		GoatVersion  string    `engine:"GoatVersion"`
+		CommandScope app.Scope `global:"CommandScope"`
+		CommandName  string    `argument:"?$1"`
+	}
 }
 
 // NewModule create new command module instance
 func NewModule() app.Module {
-	return app.Module(&Module{})
+	return &Module{}
 }
 
 // RegisterDependency is init callback to register module dependencies
-func (m *Module) RegisterDependencies(app.App) error {
+func (m *Module) RegisterDependencies(a app.App) error {
+	commandScope := a.CommandScope()
+	commandScope.Set("command.h", m.Help)
+	commandScope.Set("command.help", m.Help)
 	return nil
 }
 
@@ -37,38 +39,47 @@ func (m *Module) InitDependencies(app app.App) error {
 	if err := app.GlobalScope().InjectTo(&m.dependencies); err != nil {
 		return err
 	}
-	fmt.Println("(&m.dependencies", &m.dependencies)
+	m.app = app
 	return nil
 }
 
-// Run is run event callback
 func (m *Module) Run() error {
-	deps := m.dependencies
-	keys, err := deps.CommandScope.Keys()
+	// header
+	fmt.Println(m.dependencies.Name, " ", m.dependencies.Version)
+	if m.dependencies.Company != "" {
+		fmt.Println(m.dependencies.Company)
+	}
+	fmt.Printf("Supported by goatcore %s (%s) \n", m.dependencies.GoatVersion, "https://github.com/goatcms/goat-core")
+	if m.dependencies.Welcome != "" {
+		fmt.Printf("\n%s\n", m.dependencies.Welcome)
+	}
+	// content
+	if m.dependencies.CommandName == "" {
+		return m.Help(m.app)
+	}
+	commandIns, err := m.dependencies.CommandScope.Get("command." + m.dependencies.CommandName)
+	if err != nil || commandIns == nil {
+		fmt.Printf("Error: unknown command %s\n", m.dependencies.CommandName)
+		return nil
+	}
+	command := commandIns.(func(app.App) error)
+	return command(m.app)
+}
+
+func (m *Module) Help(app.App) error {
+	keys, err := m.dependencies.CommandScope.Keys()
 	if err != nil {
 		return err
 	}
-
-	fmt.Println(deps.Name, " ", deps.Version)
-	if deps.Welcome != "" {
-		fmt.Println(deps.Welcome)
-	}
-	fmt.Println()
-	fmt.Println("Commands:")
+	fmt.Printf("\nCommands:\n")
 	for _, key := range keys {
 		if strings.HasPrefix(key, "help.") {
-			helpStr, err := deps.CommandScope.Get(key)
+			helpStr, err := m.dependencies.CommandScope.Get(key)
 			if err != nil {
 				return err
 			}
 			fmt.Println(" ", key[5:], ": ", helpStr)
 		}
 	}
-	if deps.Company != "" {
-		fmt.Println(deps.Company)
-	}
-
-	fmt.Println()
-	fmt.Println("Developed with GoatCore (", deps.GoatVersion, ")")
 	return nil
 }
