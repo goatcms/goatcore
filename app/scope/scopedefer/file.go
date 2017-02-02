957a7b948a4d9b5bef38ff0e43d7429339a56a55
Sebastian Pozoga <sebastian@pozoga.eu>
2017-02-02 01:14:34 +0100

    add filedefer to autoremove files on fails / rollback database

---
+++ app/scope/scopedefer/file.go
@@ -0,0 +1,47 @@
+package scopedefer
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/goatcms/goat-core/app"
+	"github.com/goatcms/goat-core/filesystem"
+)
+
+type FileDefer struct {
+	files []filesystem.File
+}
+
+func (d *FileDefer) Remove(interface{}) error {
+	var errors []error
+	for _, file := range d.files {
+		if err := file.Remove(); err != nil {
+			errors = append(errors, err)
+		}
+	}
+	if len(errors) > 0 {
+		// TODO: Add suport to containing multiple errors
+		return fmt.Errorf("%v", errors)
+	}
+	return nil
+}
+
+func (d *FileDefer) Add(file filesystem.File) {
+	d.files = append(d.files, file)
+}
+
+func RemoveOn(scope app.Scope, eventID int, file filesystem.File) error {
+	var def *FileDefer
+	insKey := "_scopedefer.FileDefer:" + strconv.Itoa(eventID)
+	fileDeferIns, err := scope.Get(insKey)
+	if err != nil || fileDeferIns == nil {
+		def = &FileDefer{
+			files: []filesystem.File{file},
+		}
+		scope.On(eventID, def.Remove)
+	} else {
+		def = fileDeferIns.(*FileDefer)
+		def.Add(file)
+	}
+	return nil
+}
