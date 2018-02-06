9e1e5b57edb163ba2aebdebe9fed54be0b83177c
Sebastian Pozoga <sebastian@pozoga.eu>
2018-02-07 00:10:37 +0100

    add close support

---
+++ app/appdo/main.go
@@ -0,0 +1,26 @@
+package appdo
+
+import "github.com/goatcms/goatcore/app"
+
+// Close shutdown application by send close event
+func Close(a app.App) (err error) {
+	if err = a.AppScope().Trigger(app.BeforeCloseEvent, nil); err != nil {
+		return err
+	}
+	if err = a.AppScope().Trigger(app.CloseEvent, nil); err != nil {
+		return err
+	}
+	// no life after close ;-)
+	return nil
+}
+
+// Emit a event to all aplication scopes
+func Emit(a app.App, event int, data interface{}) {
+	a.ArgsScope().Trigger(event, nil)
+	a.ConfigScope().Trigger(event, nil)
+	a.EngineScope().Trigger(event, nil)
+	a.CommandScope().Trigger(event, nil)
+	a.DependencyScope().Trigger(event, nil)
+	a.FilespaceScope().Trigger(event, nil)
+	a.AppScope().Trigger(event, nil)
+}
