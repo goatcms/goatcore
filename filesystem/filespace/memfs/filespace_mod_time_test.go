d97c2f80d8759467b64a52e4ff7666c12081e9be
Sebastian Pozoga <sebastian@pozoga.eu>
2019-05-18 19:29:46 +0200

    add losing test file for file modification time

---
+++ filesystem/filespace/memfs/filespace_mod_time_test.go
@@ -0,0 +1,41 @@
+package memfs_test
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/goatcms/goatcore/filesystem/filespace/memfs"
+)
+
+func TestModTime(t *testing.T) {
+	var (
+		fs       *memfs.Filespace
+		err      error
+		fileInfo os.FileInfo
+		modTime  time.Time
+	)
+	t.Parallel()
+	// init
+	if fs, err = memfs.NewFilespace(); err != nil {
+		t.Error(err)
+	}
+	// Prepare test data
+	testData := []byte("There is test data")
+	path := "/mydir1/mydir2/mydir3/myfile.ex"
+	fs.WriteFile(path, testData, 0777)
+	// test data
+	if fileInfo, err = fs.Lstat(path); err != nil {
+		t.Error(err)
+	}
+	if modTime = fileInfo.ModTime(); modTime.IsZero() {
+		t.Errorf("Time %v can not be unix zero date", modTime)
+	}
+	if modTime != fileInfo.ModTime() {
+		t.Errorf("Modification time should be modified only on change")
+	}
+	fs.WriteFile(path, testData, 0777)
+	if modTime == fileInfo.ModTime() {
+		t.Errorf("Modification time should be updated on change")
+	}
+}
