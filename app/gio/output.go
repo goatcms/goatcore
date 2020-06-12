d88d3aa8d764f9d474f4db5073c9d7ed930daafb
Sebastian Pożoga <prog255@gmail.com>
2020-06-13 01:02:56 +0200

    gio: correct file names - refactor

--- app/gio/output.go
+++ app/gio/output.go
@@ -1,28 +1,44 @@
 package gio
 
 import (
+	"fmt"
+	"io"
+	"sync"
+
 	"github.com/goatcms/goatcore/app"
 )
 
-var (
-	nilOutputInstance app.Output = NilOutput{}
-)
+// Output represent system output
+type Output struct {
+	wd io.Writer // writer provided by the client
+	mu sync.Mutex
+}
 
-// NilOutput represent empty output
-type NilOutput struct{}
+// NewOutput returns a new Output.
+func NewOutput(wd io.Writer) *Output {
+	return &Output{
+		wd: wd,
+	}
+}
 
-// NewNilOutput returns a new NilOutput
-func NewNilOutput() app.Output {
-	return nilOutputInstance
+// NewAppOutput returns a new app.Output.
+func NewAppOutput(wd io.Writer) app.Output {
+	return NewOutput(wd)
 }
 
 // Printf formats according to a format specifier and writes to standard output.
 // It returns the number of bytes written and any write error encountered.
-func (out NilOutput) Printf(format string, a ...interface{}) error {
-	return nil
+func (out *Output) Printf(format string, a ...interface{}) error {
+	out.mu.Lock()
+	defer out.mu.Unlock()
+	s := fmt.Sprintf(format, a...)
+	_, err := out.wd.Write([]byte(s))
+	return err
 }
 
 // Write data to output
-func (out NilOutput) Write(p []byte) (n int, err error) {
-	return len(p), nil
+func (out *Output) Write(p []byte) (n int, err error) {
+	out.mu.Lock()
+	defer out.mu.Unlock()
+	return out.wd.Write(p)
 }
