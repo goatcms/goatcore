0d3cd9ab6193573ffb7368973945b07ea7ef6946
Sebastian Pożoga <prog255@gmail.com>
2020-06-30 02:01:53 +0200

    add plainmap.StringMapToRecursiveMap plainmap.ToRecursiveMap functions

--- varutil/plainmap/rmap.go
+++ varutil/plainmap/rmap.go
@@ -1,5 +1,11 @@
 package plainmap
 
+import (
+	"strings"
+
+	"github.com/goatcms/goatcore/varutil/goaterr"
+)
+
 // RecursiveMapToPlainMap conavert multi level map to a plain map
 func RecursiveMapToPlainMap(source map[string]interface{}) (map[string]interface{}, error) {
 	out := map[string]interface{}{}
@@ -22,4 +28,80 @@ func recursiveMapToPlainMapNode(out, source map[string]interface{}, basekey, sep
 		}
 	}
 	return nil
+}
+
+// ToRecursiveMap conavert a plain map to a multi level tree
+func ToRecursiveMap(source map[string]interface{}) (out map[string]interface{}, err error) {
+	var (
+		path []string
+		node map[string]interface{}
+	)
+	out = make(map[string]interface{})
+	for spath, value := range source {
+		if spath == "" {
+			return nil, goaterr.Errorf("ToRecursiveMap: empty key is no allowd")
+		}
+		path = strings.Split(spath, ".")
+		if len(path) > 1 {
+			if node, err = toRecursiveMapCreateNode(out, path[:len(path)-1]); err != nil {
+				return nil, err
+			}
+		} else {
+			node = out
+		}
+		node[path[len(path)-1]] = value
+	}
+	return out, nil
+}
+
+func toRecursiveMapCreateNode(rmap map[string]interface{}, path []string) (map[string]interface{}, error) {
+	var (
+		node = rmap
+		v    interface{}
+		ok   bool
+	)
+	if len(path) == 0 {
+		return nil, goaterr.Errorf("ToRecursiveMap: empty path is not allowed")
+	}
+	for i := 0; i < len(path); i++ {
+		key := path[i]
+		if v, ok = node[key]; !ok {
+			child := make(map[string]interface{})
+			node[key] = child
+			node = child
+			continue
+		}
+		switch child := v.(type) {
+		case map[string]interface{}:
+			node = child
+			continue
+		default:
+			return nil, goaterr.Errorf("%v is not a node", strings.Join(path[:i], "."))
+		}
+	}
+	return node, nil
+}
+
+// StringMapToRecursiveMap conavert a plain string map to a multi level tree
+func StringMapToRecursiveMap(source map[string]string) (out map[string]interface{}, err error) {
+	var (
+		path []string
+		node map[string]interface{}
+	)
+	out = make(map[string]interface{})
+	for spath, value := range source {
+		if spath == "" {
+			return nil, goaterr.Errorf("ToRecursiveMap: empty key is no allowd")
+		}
+		path = strings.Split(spath, ".")
+		if len(path) > 1 {
+			if node, err = toRecursiveMapCreateNode(out, path[:len(path)-1]); err != nil {
+				return nil, err
+			}
+		} else {
+			node = out
+		}
+		node[path[len(path)-1]] = value
+	}
+	return out, nil
 }
--- varutil/plainmap/rmap_test.go
+++ varutil/plainmap/rmap_test.go
@@ -55,3 +55,125 @@ func TestRMapToPMapWithJson(t *testing.T) {
 		t.Error("keymap.key11 != 11")
 	}
 }
+
+func TestToRecursiveMap(t *testing.T) {
+	t.Parallel()
+	var (
+		err      error
+		result   map[string]interface{}
+		node     map[string]interface{}
+		ok       bool
+		value    interface{}
+		intvalue int
+		svalue   string
+	)
+	if result, err = ToRecursiveMap(map[string]interface{}{
+		"dep.key":  "a",
+		"simpekey": "b",
+		"intvalue": int(11),
+	}); err != nil {
+		t.Error(err)
+		return
+	}
+	// check simpekey
+	if value, ok = result["intvalue"]; !ok {
+		t.Errorf("expected intvalue")
+		return
+	}
+	if intvalue, ok = value.(int); !ok {
+		t.Errorf("expected intvalue as int type")
+		return
+	}
+	if intvalue != 11 {
+		t.Errorf("expected intvalue equals to 11 and take %v", intvalue)
+		return
+	}
+	// check simpekey
+	if value, ok = result["simpekey"]; !ok {
+		t.Errorf("expected simpekey")
+		return
+	}
+	if svalue, ok = value.(string); !ok {
+		t.Errorf("expected simpekey as string type")
+		return
+	}
+	if svalue != "b" {
+		t.Errorf("expected dep.key equals to 'b'")
+		return
+	}
+	// check dep.key
+	if value, ok = result["dep"]; !ok {
+		t.Errorf("expected dep node")
+		return
+	}
+	if node, ok = value.(map[string]interface{}); !ok {
+		t.Errorf("expected dep as map[string]interface{} type")
+		return
+	}
+	if value, ok = node["key"]; !ok {
+		t.Errorf("expected dep.key value")
+		return
+	}
+	if svalue, ok = value.(string); !ok {
+		t.Errorf("expected dep.key as string value")
+		return
+	}
+	if svalue != "a" {
+		t.Errorf("expected dep.key equals to 'a'")
+		return
+	}
+
+}
+
+func TestStringMapToRecursiveMap(t *testing.T) {
+	t.Parallel()
+	var (
+		err    error
+		result map[string]interface{}
+		node   map[string]interface{}
+		ok     bool
+		value  interface{}
+		svalue string
+	)
+	if result, err = StringMapToRecursiveMap(map[string]string{
+		"dep.key":  "a",
+		"simpekey": "b",
+	}); err != nil {
+		t.Error(err)
+		return
+	}
+	// check simpekey
+	if value, ok = result["simpekey"]; !ok {
+		t.Errorf("expected simpekey")
+		return
+	}
+	if svalue, ok = value.(string); !ok {
+		t.Errorf("expected simpekey as string type")
+		return
+	}
+	if svalue != "b" {
+		t.Errorf("expected dep.key equals to 'b'")
+		return
+	}
+	// check dep.key
+	if value, ok = result["dep"]; !ok {
+		t.Errorf("expected dep node")
+		return
+	}
+	if node, ok = value.(map[string]interface{}); !ok {
+		t.Errorf("expected dep as map[string]interface{} type")
+		return
+	}
+	if value, ok = node["key"]; !ok {
+		t.Errorf("expected dep.key value")
+		return
+	}
+	if svalue, ok = value.(string); !ok {
+		t.Errorf("expected dep.key as string value")
+		return
+	}
+	if svalue != "a" {
+		t.Errorf("expected dep.key equals to 'a'")
+		return
+	}
+}
