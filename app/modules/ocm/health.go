81d660110280209c3c94dd76911f2fbed003a9d5
Sebastian Pożoga <prog255@gmail.com>
2020-08-21 23:29:59 +0200

    correct health checker

--- app/modules/ocm/health.go
+++ app/modules/ocm/health.go
@@ -6,19 +6,17 @@ import (
 	"github.com/goatcms/goatcore/app"
 )
 
-// SandboxHealthChecker check if sandbox contains all dependencies
-func SandboxHealthChecker(a app.App, ctxScope app.Scope) (msg string, err error) {
+// ContainerHealthChecker check if container provider is installed
+func ContainerHealthChecker(a app.App, ctxScope app.Scope) (msg string, err error) {
 	if !hasPodman && !hasDocker {
 		err = goaterr.Errorf("Docker or podman are required. Installed docker ( https://docs.docker.com/get-docker/ ) or podman ( https://podman.io/getting-started/installation )")
 		return err.Error(), err
 	}
 	if hasDocker && !hasPodman {
-		err = goaterr.Errorf("Docker is available. (Warning: podman is unavailable for oc.engime=podman)")
-		return err.Error(), err
+		return "Docker is available. (Warning: podman is unavailable for oc.engime=podman)", nil
 	}
 	if !hasDocker && hasPodman {
-		err = goaterr.Errorf("Podman is available. (Warning: docker is unavailable for oc.engime=docker)")
-		return err.Error(), err
+		return "Podman is available. (Warning: docker is unavailable for oc.engime=docker)", nil
 	}
 	return "Docker and podman are available. You can set container system by oc.engine=docker|podman attribute). Default container system is podman (as more secure).", nil
 }
